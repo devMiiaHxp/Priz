8d2db4dde5fb5ed7af274fdc9586f21f4e172f49
devMiiaHxp <dopemiiahxy@gmail.com>
2024-02-13 18:59:30 +0100

    removed usa flag

--- src/components/Partnership.jsx
+++ src/components/Partnership.jsx
@@ -12,7 +12,7 @@ export const Partnership = () => {
         <div className=" items-center justify-between gap-20 flex flex-col lg:grid lg:grid-cols-10 my-10">
           <div className="col-span-5 rounded-lg overflow-hidden   ">
             <img
-              src="./assets/partner.jpeg"
+              src="./assets/partner.jpg"
               className="w-[100%] h-[100%]"
               alt=""
             />
